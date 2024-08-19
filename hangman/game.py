537881c8c94b3778d49e4b25805d65ab286fc4ca
GAlthobaity <gealthobaity@gmail.com>
2024-08-19 17:58:21 +0300

    UPLOAD: basic game file without exceptions.

--- hangman/game.py
+++ hangman/game.py
@@ -1,24 +1,27 @@
-from .exceptions import *
-
-# Complete with your own, just for fun :)
+# from .exceptions import *
+import random
 LIST_OF_WORDS = []
 
-
 def _get_random_word(list_of_words):
-    pass
-
+    return random.choice(list_of_words)
 
 def _mask_word(word):
-    pass
+    return '*'*len(word) 
 
-
-def _uncover_word(answer_word, masked_word, character):
-    pass
-
+def _uncover_word(answer_word, masked_word, letter):
+    return ''.join(
+        letter if answer_word[i] == letter else masked_letter
+        for i, masked_letter in enumerate(masked_word)
+    )
 
 def guess_letter(game, letter):
-    pass
-
+        new_masked_word = _uncover_word(game['answer_word'], game['masked_word'], letter)
+        if game['masked_word'] == new_masked_word:
+            game['remaining_misses'] -= 1
+        else:
+            game['masked_word'] = new_masked_word
+        
+        game['previous_guesses'].append(letter)
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
     if list_of_words is None:
